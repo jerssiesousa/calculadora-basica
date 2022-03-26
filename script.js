f2f304ba6fd84cee88d26b8eb6b531126ff104ae
Jerssie <jerssiers@gmail.com>
2022-03-25 22:19:14 -0300

    add funcao result

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function digitar(x){
     input.value += x ;
 
 }
+
+function result(){
+    let input=document.querySelector("[name='txt1']");
+    let expressao = eval(input.value);
+    input.value = expressao
+}
+function limpar(){
+    console.log('Limpar')
+}
