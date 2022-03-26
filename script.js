8048855c8779dd80b32f9534b1f6a072303563ea
Jerssie <jerssiers@gmail.com>
2022-03-25 23:24:25 -0300

    add modo escuro em js e css

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-let input=document.querySelector("[name='txt1']");
+let valor=document.querySelector("[name='txt1']");
 
 function digitar(x){
 
-    input.value += x ;
+    valor.value += x ;
 
-}
+};
 
 function result(){
     
-    let expressao = eval(input.value);
-    input.value = expressao
-}
+    let expressao = eval(valor.value);
+    valor.value = expressao
+};
+
 function limpar(){
 
-    input.value = ""
+    valor.value = ""
     
+};
+
+const botao = document.getElementsByName("botao");
+const form = document.querySelector("form");
+const body = document.querySelector("body");
+const darkModeClass = 'dark-mode';
+
+
+function changeMode() {
+    valor.classList.toggle(darkModeClass);
+    form.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+
+    for(let i = 0; i < botao.length; i++){
+        botao[i].classList.toggle(darkModeClass);
+    };
 }
