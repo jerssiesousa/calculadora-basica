74120a82bb88d0ad48f8df9f0661b0ab32faebbc
Jerssie <jerssiers@gmail.com>
2022-03-26 19:45:01 -0300

    adicionando o README e comentários no JS

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# calculadora-basica
-Criando uma calculadora de uso cotidiano
+# Calculadora em HTML5/CSS3/JS
+
+* A ideia é contruir uma calculadora comum desde de seu desenho à sua funcionalidade. Assim aprimorar os conhecimentos nas presentes linguagens. 
+<img src="./img/calc-light.png" alt="calculadora-modo-claro" style="width: 300px;">
+
+* Inicialmente foi pensado em construir os botões em formato de tabela em HTML, contudo se viu uma oportunidade de prática no posicionamento em CSS. Assim o posicionamento está todo em CSS.
+
+* O JavaScript foi usado para aumentar o conhecimento sobre a relação com o DOM, para usar o comando toggle foi acrescentado o modo escuro da calculadora com o apoio do CSS.
+
+<img src="./img/calc-dark.png" alt="calculadora-modo-escuro" style="width: 300px;">
+
+* O uso do paramentro foi essencial para funcionalidade dos botões e as funções
+
+* Para entrar em contato use o email : <a href="mailto:jerssiers@gmail.com"> Jerssie Sousa</a>
--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 let valor=document.querySelector("[name='txt1']");
+// variável para pegar o valor do visor
+
 
 function digitar(x){
 
     valor.value += x ;
 
 };
+// Funcão pra digitar no próprio DOM
+
 
 function result(){
     
     let expressao = eval(valor.value);
     valor.value = expressao
 };
+// Função para pegar o valor no visor(input) passar a string pra uma expressão com operadores e números
 
 function limpar(){
 
     valor.value = ""
     
 };
+// Função para limpar o visor
 
 const botao = document.getElementsByName("botao");
 const form = document.querySelector("form");
 const body = document.querySelector("body");
 const darkModeClass = 'dark-mode';
-
+// Constantes para produzir o modo escuro
 
 function changeMode() {
     valor.classList.toggle(darkModeClass);
     form.classList.toggle(darkModeClass);
     body.classList.toggle(darkModeClass);
+    // toggle é usado para retirar e colocar a classe dos elementos que recebem cor
 
     for(let i = 0; i < botao.length; i++){
         botao[i].classList.toggle(darkModeClass);
     };
+    //for usado para pegar todos o elementos input de type:button
 }
