ed47c2d5720d2d69dee2f0386223d9336e1fd9fc
Jerssie <jerssiers@gmail.com>
2022-03-25 22:23:22 -0300

    add funcao clear

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+let input=document.querySelector("[name='txt1']");
+
 function digitar(x){
 
-    let input=document.querySelector("[name='txt1']");
     input.value += x ;
 
 }
 
 function result(){
-    let input=document.querySelector("[name='txt1']");
+    
     let expressao = eval(input.value);
     input.value = expressao
 }
 function limpar(){
-    console.log('Limpar')
+
+    input.value = ""
+    
 }
